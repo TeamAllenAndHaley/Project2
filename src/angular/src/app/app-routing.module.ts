877f82c49d4bf76ec20b746c80422b568a20a782
Allen Nguyen <allendewy@gmail.com>
2018-07-23 23:21:20 -0400

    add home and login components; add auth-guard and auth services; add routing module

--- src/angular/src/app/app-routing.module.ts
+++ src/angular/src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuardService } from './service/auth-guard.service';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{
+    path: '',
+    component: HomeComponent//,
+    // canActivate: AuthGuardService,
+    // children: [{
+    //   path: 'test',
+    //   component: null
+    // }]
+  }, {
+    path: 'login',
+    component: LoginComponent
+  }];
+
 @NgModule({
-  exports: [
-    RouterModule
-  ]
+  imports:[ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }

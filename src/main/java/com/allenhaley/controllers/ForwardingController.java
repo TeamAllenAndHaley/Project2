ac7eb4bfe2e935f33df82efd350f143b3e364f0b
Allen Nguyen <allendewy@gmail.com>
2018-07-25 16:32:37 -0400

    attempt at spa routing forward to index

--- src/main/java/com/allenhaley/controllers/ForwardingController.java
+++ src/main/java/com/allenhaley/controllers/ForwardingController.java
@@ -1,12 +1,26 @@
 package com.allenhaley.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * TODO: Find solution to allow angular's routing to work alongside /api/*
+ */
 
 @Controller
+@RequestMapping(path = "/testing")
 public class ForwardingController {
-    @RequestMapping(value = "/**/{path:[^\\.]+}")
-    public String html5Forwarding() {
-        return "forward:/";
-    }
+//    @GetMapping(path = "/login")
+//    public String html5Forwarding() {
+//        return "forward:/index.html";
+//    }
+//
+//    @GetMapping(path = "/test")
+//    @ResponseBody
+//    public String testController() {
+//        System.out.println("Hello test.");
+//        return "Hello, Test!";
+//    }
 }

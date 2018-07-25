ded6c119c234eb84876051452d8b58786e6e12ea
hhodges10 <hhodges10@msn.com>
2018-07-25 16:56:54 -0400

    working on helloworld controller

--- src/main/java/com/allenhaley/controllers/ForwardingController.java
+++ src/main/java/com/allenhaley/controllers/ForwardingController.java
@@ -3,6 +3,7 @@ package com.allenhaley.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**

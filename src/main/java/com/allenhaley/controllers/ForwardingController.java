89c3715a06bf8e322b1b6beb8f1a917e1006c51d
Allen Nguyen <allendewy@gmail.com>
2018-07-25 23:30:25 -0400

    merge conflicts and move webconfig/init out of /util directory

--- src/main/java/com/allenhaley/controllers/ForwardingController.java
+++ src/main/java/com/allenhaley/controllers/ForwardingController.java
@@ -3,7 +3,6 @@ package com.allenhaley.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -11,17 +10,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 
 @Controller
-@RequestMapping(path = "/testing")
+@RequestMapping("/")
 public class ForwardingController {
 //    @GetMapping(path = "/login")
 //    public String html5Forwarding() {
 //        return "forward:/index.html";
 //    }
 //
-//    @GetMapping(path = "/test")
-//    @ResponseBody
-//    public String testController() {
-//        System.out.println("Hello test.");
-//        return "Hello, Test!";
-//    }
+    @GetMapping("/")
+    @ResponseBody
+    public String testMethod() {
+        System.out.println("Hello base.");
+        return "Hello, Base!";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String testAlso() {
+        System.out.println("Hello test.");
+        return "Hello, Test!";
+    }
 }

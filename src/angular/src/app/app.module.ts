34feaa810847398ce6c9beaff6dd2d995a6a6fad
Allen Nguyen <allendewy@gmail.com>
2018-07-28 18:17:58 -0400

    non-functional login user interface

--- src/angular/src/app/app.module.ts
+++ src/angular/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -25,7 +29,12 @@ import { TicketComponent } from './home/submit/ticket/ticket.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatInputModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/angular/src/app/login/login.component.ts
+++ src/angular/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from 'node_modules/@angular/forms';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  password: FormControl = new FormControl('', [Validators.required]);
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  getEmailError(): String {
+    return this.email.hasError('required') ? 'email is required to login' :
+        this.email.hasError('email') ? 'entered email is invalid' : '';
+  }
+
+  signIn(): void {
+    console.log('signin runs!');
   }
 
 }

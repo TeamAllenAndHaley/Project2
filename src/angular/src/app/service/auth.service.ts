a57c6fea080c799a8382ba261ed2306214954883
Allen Nguyen <allendewy@gmail.com>
2018-07-29 10:08:43 -0400

    merge master and tidy up

--- src/angular/src/app/service/auth.service.ts
+++ src/angular/src/app/service/auth.service.ts
@@ -7,21 +7,27 @@ import { HttpClient } from 'node_modules/@angular/common/http';
 export class AuthService {
   constructor(private httpClient: HttpClient) { }
 
-  public login(id: string, pw: string) {
-    console.log(id + ' ' + pw);
+ login(id: string, pw: string) {
     let url = 'http://localhost:8100/api/login/auth';
-    return this.httpClient.post(url, {"email":id, "password":pw}).subscribe(
+
+    return this.httpClient.post(url, {
+      email: id,
+      password: pw
+    }).subscribe(
       res => console.log(res)
     );
-    // return this.httpClient.get(url).subscribe(res => console.log("test"));
   }
 
   isNewUser(id: string): boolean {
-    // get account by email
-    // if exists, get password
-    //    if password is empty/null, return true
+    // get password by email
+    // if exists && password is empty/null
+    //    return true
     // else return false
     return true;
+  }
+
+  register(id: string, pw: string) {
+    // set password for user
   }
 
   logout() {}
